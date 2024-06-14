0d838afcd7aee4efc5738184f08d50ad03fb1e0f
Johan Zevallos <jzevallos@evoltacrm.com>
2024-06-13 19:52:38 -0500

    Agregando ambito_bloque.js

--- ambito_bloque.js
+++ ambito_bloque.js
@@ -3,7 +3,11 @@
 let mascota_perro = "lomito";
 console.log(mascota_perro)
 
+mascota_gato = 100
+console.log(mascota_gato);
 //Ambito
 {
     let mascota_perro = "Kanela"
+    console.log(mascota_perro)
 }
+console.log(mascota_perro)
